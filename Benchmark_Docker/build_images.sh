215d48bcd90e8dc8113d1bf4594480f53f222681
bryceshirley <bryce.shirley@stfc.ac.uk>
2024-08-12 18:33:13 +0000

    dummy container added

--- Benchmark_Docker/build_images.sh
+++ Benchmark_Docker/build_images.sh
@@ -10,9 +10,13 @@ docker build -t gpu_base -f Benchmark_Dockerfiles/Dockerfile.gpu_base .
 echo "Building sciml_base image..."
 docker build -t sciml_base -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.sciml_base .
 
-# # Build mantid_base image using gpu_base as base
-# echo "Building mantid_base image..."
-# docker build -t mantid_base -f Benchmark_Dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_base .
+# Build mantid_base image using gpu_base as base
+echo "Building mantid_base image..."
+docker build -t mantid_base -f Benchmark_Dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_base .
+
+# Build stemdl_classification image using sciml_base as base
+echo "Building Sciml Benchmark: stemdl_classification_base image..."
+docker build -t stemdl_classification_base -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.stemdl_classification_base .
 
 ## ~~~~~~~~~ Build Sciml Benchmark Images ~~~~~~~~~
 echo "Building Sciml Benchmark images..."
@@ -22,6 +26,10 @@ echo "Building Sciml Benchmark: synthetic_regression image..."
 docker build -t synthetic_regression -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.synthetic_regression .
 
 # Build stemdl_classification image using sciml_base as base
+echo "Building Sciml Benchmark: stemdl_classification_1gpu image..."
+docker build -t stemdl_classification_2gpu -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.stemdl_classification_1gpu .
+
+# Build stemdl_classification image using sciml_base as base
 echo "Building Sciml Benchmark: stemdl_classification_2gpu image..."
 docker build -t stemdl_classification_2gpu -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.stemdl_classification_2gpu .
 
@@ -29,15 +37,19 @@ docker build -t stemdl_classification_2gpu -f Benchmark_Dockerfiles/sciml_benchm
 echo "Building Sciml Benchmark: mnist_tf_keras image..."
 docker build -t mnist_tf_keras -f Benchmark_Dockerfiles/sciml_benchmarks/Dockerfile.mnist_tf_keras .
 
-# ## ~~~~~~~~~ Build Mantid Imaging Benchmark Images ~~~~~~~~~
-# echo "Building Mantid Imaging Benchmark images..."
+## ~~~~~~~~~ Build Mantid Imaging Benchmark Images ~~~~~~~~~
+echo "Building Mantid Imaging Benchmark images..."
 
-# # Build mantid_run_8 image using sciml_base as base
-# echo "Building Mantid Imaging Benchmark: mantid_run_8 image..."
-# docker build -t mantid_run_1 -f Dockerfiles/benchmark_dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_run_8 .
+# Build mantid_run_8 image using sciml_base as base
+echo "Building Mantid Imaging Benchmark: mantid_run_8 image..."
+docker build -t mantid_run_1 -f Benchmark_Dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_run_8 .
 
-# # Build mantid_run_1 image using sciml_base as base
-# echo "Building Mantid Imaging Benchmark: mantid_run_1 image..."
-# docker build -t mantid_run_8 -f Dockerfiles/benchmark_dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_run_1 .
+# Build mantid_run_1 image using sciml_base as base
+echo "Building Mantid Imaging Benchmark: mantid_run_1 image..."
+docker build -t mantid_run_8 -f Benchmark_Dockerfiles/mantid_imaging_benchmarks/Dockerfile.mantid_run_1 .
+
+## ~~~~~~~~~ Building Dummy Container Images ~~~~~~~~~
+echo "Building Dummy Container image..."
+docker build -t dummy_container -f Benchmark_Dockerfiles/dummy/Dockerfile.dummy_container .
 
 echo -e "Build process completed.\n"
