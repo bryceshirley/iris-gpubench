a04cc984e6cc8578e914acee5fe1b2ad78b6c19d
bryceshirley <bryceshirley@stfc.ac.uk>
2024-08-19 08:11:56 +0000

    Docstring added

--- dockerfiles/pull_images.sh
+++ dockerfiles/pull_images.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# ==============================================================================
+# Pull Docker Images from GitHub Container Registry
+# ==============================================================================
+#
+# This script pulls Docker images from GitHub Container Registry based on the
+# Dockerfiles located in the specified directory. Each Dockerfile's extension
+# is used to determine the corresponding image name.
+#
+# Requirements:
+# - Docker must be installed and running.
+# - Access to GitHub Container Registry with appropriate permissions.
+#
+# Usage:
+# Run the script: ./pull_images.sh
+#
+# Author: Bryce Shirley
+# Date: 19.08.2024
+# ==============================================================================
+
 # Set the GitHub Container Registry base URL and repository
 REGISTRY_URL="ghcr.io"
 REPOSITORY="bryceshirley/iris-gpubench"
